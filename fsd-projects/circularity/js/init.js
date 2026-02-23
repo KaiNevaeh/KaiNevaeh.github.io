df28a898bc3da00208a4b39e1961915d092fb26f
KaiNevaeh <kai.jones@inspire35.org>
2026-02-23 15:37:25 +0000

    add ASD project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,21 +20,29 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-
-
+var circle;
+var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        
+        function drawCircle(){
+            var circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
+physikz.addRandomVelocity(circle, canvas, 5, 5);
+view.addChild(circle);
+circles.push(circle);
 
 
         // TODO 3 : Call the drawCircle() function
-
+drawCircle();
+drawCircle();
+drawCircle();
+drawCircle();
+drawCircle();
 
 
         // TODO 7 : Use a loop to create multiple circles
-
-
-
+for (var i = 0; i < 100; i++) {
+  drawCircle();
+}
 
         ///////////////////
         // PROGRAM LOGIC //
@@ -47,14 +55,26 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the position of each circle using physikz.updatePosition()
-
+physikz.updatePosition(circles[0]);
+physikz.updatePosition(circles[1]);
+physikz.updatePosition(circles[2]);
+physikz.updatePosition(circles[3]);
+physikz.updatePosition(circles[4]);
             
             // TODO 5 : Call game.checkCirclePosition() on your circles
-           
+           game.checkCirclePosition(circles[0]);
+game.checkCirclePosition(circles[1]);
+game.checkCirclePosition(circles[2]);
+game.checkCirclePosition(circles[3]);
+game.checkCirclePosition(circles[4]);
 
             // TODO 8 / TODO 9 : Iterate over the array
-           
-            
+           function update() {
+               for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+    }
+}
         }
     
         /* 
@@ -70,9 +90,19 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+            if (circleX > width + circleRadius) {
+  circleX = -circleRadius;
+if (circleX < -circleRadius) {
+    circleX = width + circleRadius;
+}
+if (circleY > height + circleRadius) {
+    circleY = -circleRadius;
+    }
 
+if (circleY < -circleRadius) {
 
+  circleY = height + circleRadius;
+}
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
         
